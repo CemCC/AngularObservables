fc018e23972f17823f9bf0469d0c9c4db3588b7f
CemCC <cemcaliskan@ymail.com>
2018-07-08 22:42:43 +1000

    Select default value onthe list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.selections$ = this.selectionService.getSelections();
-   //this.defaultOption = 'number2';
+    this.defaultOption = 'number1';
   }
 
   /*private searchSelections( selections: Selection[]){
